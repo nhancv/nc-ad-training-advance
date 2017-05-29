c085540f87c2e124b5cadd61df151f82f715a08e
Nhan Cao <nhancv92@gmail.com>
2017-05-29 14:14:52 +0700

    config multi dex

---
+++ app/src/main/java/com/nhancv/training/MainApplication.java
@@ -0,0 +1,13 @@
+package com.nhancv.training;
+
+import android.support.multidex.MultiDexApplication;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by nhancao on 5/29/17.
+ */
+
+@EApplication
+public class MainApplication extends MultiDexApplication {
+}
