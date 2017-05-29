f9cb6e69198ede54c76a7a303e90a19024f18499
Nhan Cao <nhancv92@gmail.com>
2017-05-29 14:08:01 +0700

    integrate android annotations

--- app/src/main/java/com/nhancv/training/MainActivity.java
+++ app/src/main/java/com/nhancv/training/MainActivity.java
@@ -1,13 +1,10 @@
 package com.nhancv.training;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
